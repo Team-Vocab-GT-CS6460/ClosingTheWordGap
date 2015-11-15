f0b98bb02b337638dbeab90be40e2a7fc4d3489b
sansonen <sansonen@gmail.com>
2015-11-15 01:11:31 -0500

    removed duplicate questions and answers

--- src/main/java/edu/gatech/wordgap/spring/jdbc/model/VocabQuizQuestion.java
+++ src/main/java/edu/gatech/wordgap/spring/jdbc/model/VocabQuizQuestion.java
@@ -2,9 +2,16 @@ package edu.gatech.wordgap.spring.jdbc.model;
 
 public class VocabQuizQuestion {
 	private String question;
+	private String ttsString;
 	private int correctAnswer;
 	private VocabQuizAnswer[] answers;
 	
+	public String getTtsString() {
+		return ttsString;
+	}
+	public void setTtsString(String ttsString) {
+		this.ttsString = ttsString;
+	}
 	public String getQuestion() {
 		return question;
 	}
